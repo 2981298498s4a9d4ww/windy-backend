e29d0c58ddab18954ba20597db1904cff2bfa0cd
2981298498s4a9d4ww <codemagician8@gmail.com>
2026-01-04 09:34:45 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const WINDY_KEY = "PUT_KEY_LATER";
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.get("/webcams", async (req, res) => {
+  try {
+    const url = new URL("https://api.windy.com/webcams/api/v3/list");
+
+    url.searchParams.set("limit", "10");
+    url.searchParams.set("sortKey", "popularity");
+    url.searchParams.set("sortDirection", "desc");
+    url.searchParams.set("include", "images,player");
+    url.searchParams.set("countries", "US");
+    url.searchParams.set("categories", "city");
+
+    const response = await fetch(url.toString(), {
+      headers: {
+        "x-windy-api-key": WINDY_KEY
+      }
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
